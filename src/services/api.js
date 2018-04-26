471a641d24ac1aa4593c35364494c5b5d3502989
Laucha <lautaro.bo@wolox.com.ar>
2018-04-26 12:48:28 -0300

    Index to Vue + BS fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import { create } from 'apisauce'
 
 const api = create({
-  baseURL: 'http://demo6w061045.mockable.io'
+  baseURL: 'https://demo6061045.mockable.io'
 })
 
 export default api
--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,19 @@ module.exports = {
           'pug-html-loader'
         ]
       },
-      // TODO: Lint the app before building?
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            cache: true,
+            emitError: true,
+            emitWarning: true
+          }
+        }
+      },
       {
         test: /\.js$/,
         exclude: ['node_modules'],
