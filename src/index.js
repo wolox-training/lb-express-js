6a9145c45ea5d032e40f0ef44e034a4245af3851
Laucha <lautaro.bo@wolox.com.ar>
2018-04-27 12:42:14 -0300

    Tech to component

--- src/index.js
+++ src/index.js
@@ -23,3 +23,15 @@ const vm = new Vue({
     }
   }
 })
+
+Vue.component('tech', {
+  props: ['tech'],
+  template: `
+    <div class="tech">
+      <img class="tech-img" :src="this.tech.logo">
+      <h2 class="tech-title">{{ this.tech.name }}</h2>
+      <p class="tech-release">{{ this.tech.release }}</p>
+      <p>{{ this.tech.description }}</p>
+    </div>
+  `
+})
